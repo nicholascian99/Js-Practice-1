7199241f27fa7d53241f94d448cad25c4271fb54
Nicholas <nicholascian99@gmail.com>
2022-11-30 08:29:08 -0500

    Finished course!!

--- js/index.js
+++ js/index.js
@@ -382,7 +382,8 @@
 
 // console.log(data)
      
-
+// ________________________________________________________
+// GENERATING RANDOM NUMBERS
 // function randomFraction() {
 
 //     return Math.random();
@@ -390,15 +391,325 @@
 
 // console.log(randomFraction());
 
-let randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+// let randomNumberBetween0and19 = Math.floor(Math.random() * 20);
 
-let randomWholeNum = () => {
+// let randomWholeNum = () => {
     
     
-    return Math.floor(Math.random() * 10);
-}
+//      return Math.floor(Math.random() * 10);
+//  }
 
-console.log(randomWholeNum());
+// console.log(randomWholeNum());
+
+// let ourRandomRange = (ourMin, ourMax) => {
+//     return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+// }
+
+// ourRandomRange(1, 9);
+
+
+
+// let randomRange = (myMin, myMax) => {
+
+//     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+// }
+
+// let myRandom = randomRange(5,15);
+
+// console.log(myRandom)
+
+// ___________________________________________________________
+// PARSE INT Function
+
+
+// function convertToInteger(str) {
+//     return parseInt(str);
+
+// }
+
+// convertToInteger("56")
+
+// function convertToInteger(str) {
+//     return parseInt(str, 2)
+// }
+
+// convertToInteger("10011");
+
+// _______________________________________________________________
+// TURNARY OPERATOR
+
+// function checkEqual(a,b) {
+//     return a === b ? true : false;
+// }
+
+// checkEqual(1, 2);
+
+// function checkSign(num) {
+//     return num > 0 ? "positive" : num < 0 ? "negative" : "zero"
+// }
+
+// console.log(checkSign(0));
+
+// ________________________________________________________
+// MUTATE AN ARRAY DECLARED WITH CONST
+
+// const s = [5, 7, 8];
+
+// function editInPlace() {
+//     "use strict";
+
+//     s[0] = 2; 
+//     s[1] = 3;
+//     s[2] = 4;
+
+// }
+
+// editInPlace();
+
+// console.log(s)
+
+// _______________________________________________________
+// ARROW FUNCTIONS
+
+
+// //convert to arrow function
+// var myConcat = function (arr1, arr2) {
+//     return arr1.concat(arr2);
+// };
+
+// //converted to arrow function
+// const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+
+
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+// const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+// const squareList = (arr) => {
+//     const squaredIntegers = arr;
+//     return squaredIntegers;
+// };
+
+// const squaredIntegers = squareList(realNumberArray);
+// console.log(squaredIntegers);
+
+// const myRealNumberArray = [6, 4.6, 7.7, 16, 18, -8, 1, 4]
+    
+
+// const mySquaredList = (arr) => {;
+//     const mySquaredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+//     return mySquaredIntegers;
+// };
+
+// const mySquaredIntegers = mySquaredList(myRealNumberArray);
+// console.log(mySquaredIntegers);
+
+// _______________________________________________________
+//DEFAULT PARAMETERS
+
+// const increment = (function() {
+//     return function increment(number, value) {
+//         return number + value;
+//     };
+// })();
+// console.log(increment(5, 2));
+// console.log(increment(5));
+
+// __________________________________________________________
+// REST OPERATOR
+
+// const sum = (function(){
+//     return function sum(...args) {
+//         return args.reduce((a,b) => a + b, 0);
+//     };
+// })();
+// console.log(sum(1, 2, 3, 4));
+
+// ________________________________________________________
+// SPREAD OPERATOR
+
+// const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+// let arr2;
+// (function() {
+//     arr2 = [...arr1];
+//     arr1[0] = 'potato'
+// })();
+// console.log(arr2);
+
+// ________________________________________________________
+// DESTRUCTURING ASSIGNMENT
+
+// var voxel = {x: 3.6, y: 7.4, z: 6.54 };
+
+// var x = voxel.x;
+// var y = voxel.y;
+// var z = voxel.z;
+
+// const { x : a, y : b, z : c } = voxel;
+
+// ------------------------------------------------
+
+// const AVG_TEMPERATURES =  {
+//     today: 77.5,
+//     tomorrow: 79
+// };
+
+// function getTempOfTmrw(avgTemperatures) {
+//     "use strict";
+
+//     const { tomorrow : tempOfTomorrow } = avgTemperatures;
+
+//     return tempOfTomorrow;
+// }
+
+// console.log(getTempOfTmrw(AVG_TEMPERATURES));
+
+// console.log(AVG_TEMPERATURES.tomorrow)
+
+// -------------------------------------------------------
+//NESTED DESTRUCTURING ASSIGNMENT
+
+// const LOCAL_FORECAST = {
+//     today: { min: 72, max: 83 },
+//     tomorrow: { min: 73.3, max: 84.6 }
+// };
+
+// function getMaxOfTmrw(forecast) {
+//     "use strict";
+
+
+// const { tomorrow : {max : maxOfTomorrow}} = forecast;
+
+// return maxOfTomorrow;
+// }
+
+// console.log(getMaxOfTmrw(LOCAL_FORECAST));
+
+
+// -----------------------------------------------------------
+
+// const [z, x, , y] = [1, 2, 3, 4, 5, 6];
+// console.log(z, x, y);
+
+
+
+// let a = 8, b=6;
+// (() => {
+//     "use strict";
+//     [a, b] = [b, a]
+
+// })();
+
+// -----------------------------------------------------
+// DESTRUCTURING WITH THE REST OPERATOR
+
+// const source = [1,2,3,4,5,6,7,8,9,10];
+// function removeFirstTwo(list) {
+
+//     const [ , , ...arr] = list;
+
+//     return arr;
+// }
+// const arr = removeFirstTwo(source);
+// console.log(arr);
+// console.log(source);
+
+// ----------------------------------------------------------
+// USE DESTRUCTURING ASSIGNMENT TO PASS AN OBJECT AS A FUNCTION'S PARAMETERS
+
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+
+};
+const half = (function() {
+    
+    return function half({ max, min}) {
+        return (max + min) / 2.0;
+    };
+
+})();
+console.log(stats);
+console.log(half(stats));
+
+// ___________________________________________________________
+// CREATE STRINGS USING TEMPLATE LITERALS
+
+// const person = {
+//     name : "Zodiac Hasbro",
+//     age : 56
+// };
+
+// const greeting = `Hello, my name is ${person.name}!
+// I am ${person.age} years old.`;
+
+// console.log(greeting);
+
+// ---------------------------------------------------
+
+// SIMPLE FIELDS
+
+// const createPerson = (name, age, gender) => {
+
+//     return {
+//         name: name,
+//         age: age,
+//         gender: gender
+
+//     };
+// //can change all of this into what is below--------
+
+// };
+
+// const createHuman = (name, age, gender) => ( {name, age, gender});
+
+
+// console.log(createPerson("Zodiac Hasbro", 56, "male"));
+// console.log(createHuman("Binki lazbro", 78, "male"))
+
+// ____________________________________________________
+//CLASS SYNTAX(CREATES AN OBJECT IN ES6)
+
+// function makeClass() {
+//     class Vegetable {
+//         constructor(name) {
+//             this.name = name;
+//         }
+//     }
+//     return Vegetable;
+// }
+// const Vegetable = makeClass();
+// const carrot = new Vegetable('carrot');
+// console.log(carrot.name);
+
+// _________________________________________________________
+// GETTERS AND SETTERS
+
+// function makeClass() {
+//     class Thermostat {
+//         constructor(temp) {
+//             this._temp = 5/9 * (temp - 32);
+//         }
+//         get temperature(){
+//             return this._temp;
+//         }
+//         set temperature(updatedTemp){
+//             this._temp = updatedTemp;
+//         }
+//     }
+//     return Thermostat;
+// }
+
+// const Thermostat = makeClass();
+// const thermos = new Thermostat(76);
+// let temp = thermos.temperature;
+// thermos.temperature = 26;
+// temp = thermos.temperature; 
 
 
 
